fcd9a816768c9b109521d9b27a22765c01481ce2
Isaac Wengler <iwengler3@gmail.com>
2021-01-03 00:56:54 -0600

    added smooth scrool js

--- app.js
+++ app.js
@@ -17,5 +17,61 @@ const hideMobileMenu = () => {
     }
 }
 
-menuLinks.addEventListener('click', hideMobileMenu);
-navLogo.addEventListener('click', hideMobileMenu);
+function smoothScroll(target,duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top - 80;
+    var startPosition = window.pageYOffset;
+    var startTime = null;
+    function animation(currentTime) {
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, targetPosition, duration);
+        window.scrollTo(0,run);
+        if(timeElapsed < duration) {
+            window.requestAnimationFrame(animation)       || 
+            window.webkitRequestAnimationFrame(animation) || 
+            window.mozRequestAnimationFrame(animation)    || 
+            window.oRequestAnimationFrame(animation)      || 
+            window.msRequestAnimationFrame(animation)     
+        }
+    }
+
+    function ease(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+var logo = document.querySelector('#navbar__logo');
+var home = document.querySelector('#home-page');
+var about = document.querySelector('#about-page');
+var projects = document.querySelector('#projects-page');
+var mywork = document.querySelector('.main__btn');
+
+logo.addEventListener('click', function(){
+    hideMobileMenu();
+    smoothScroll('#home', 500);
+});
+
+home.addEventListener('click', function(){
+    hideMobileMenu();
+    smoothScroll('#home', 500);
+});
+
+about.addEventListener('click', function(){
+    hideMobileMenu();
+    smoothScroll('#about', 500);
+});
+
+projects.addEventListener('click', function(){
+    hideMobileMenu();
+    smoothScroll('#projects', 500);
+});
+
+mywork.addEventListener('click', function(){
+    smoothScroll('#projects', 500);
+});
