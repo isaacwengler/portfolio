01ee0121d614e38b19c6c558fe1063216d35b9e8
Isaac Wengler <iwengler3@gmail.com>
2021-12-28 00:02:37 -0600

    demos integrated into app

--- app.js
+++ app.js
@@ -11,36 +11,36 @@ menu.addEventListener('click', mobileMenu);
 
 const hideMobileMenu = () => {
     const menuBars = document.querySelector('.is-active');
-    if(window.innerWidth <= 900 && menuBars) {
+    if (window.innerWidth <= 900 && menuBars) {
         menu.classList.toggle('is-active');
         menuLinks.classList.remove('active');
     }
 }
 
-function smoothScroll(target,duration) {
+function smoothScroll(target, duration) {
     var target = document.querySelector(target);
     var targetPosition = target.getBoundingClientRect().top - 80;
     var startPosition = window.pageYOffset;
     var startTime = null;
     function animation(currentTime) {
-        if(startTime === null) startTime = currentTime;
+        if (startTime === null) startTime = currentTime;
         var timeElapsed = currentTime - startTime;
         var run = ease(timeElapsed, startPosition, targetPosition, duration);
-        window.scrollTo(0,run);
-        if(timeElapsed < duration) {
-            window.requestAnimationFrame(animation)       || 
-            window.webkitRequestAnimationFrame(animation) || 
-            window.mozRequestAnimationFrame(animation)    || 
-            window.oRequestAnimationFrame(animation)      || 
-            window.msRequestAnimationFrame(animation)     
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) {
+            window.requestAnimationFrame(animation) ||
+                window.webkitRequestAnimationFrame(animation) ||
+                window.mozRequestAnimationFrame(animation) ||
+                window.oRequestAnimationFrame(animation) ||
+                window.msRequestAnimationFrame(animation)
         }
     }
 
     function ease(t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
         t--;
-        return -c/2 * (t*(t-2) - 1) + b;
+        return -c / 2 * (t * (t - 2) - 1) + b;
     }
 
     requestAnimationFrame(animation);
@@ -52,26 +52,37 @@ var about = document.querySelector('#about-page');
 var projects = document.querySelector('#projects-page');
 var mywork = document.querySelector('.main__btn');
 
-logo.addEventListener('click', function(){
+logo.addEventListener('click', function () {
     hideMobileMenu();
     smoothScroll('#home', 500);
 });
 
-home.addEventListener('click', function(){
+home.addEventListener('click', function () {
     hideMobileMenu();
     smoothScroll('#home', 500);
 });
 
-about.addEventListener('click', function(){
+about.addEventListener('click', function () {
     hideMobileMenu();
     smoothScroll('#about', 500);
 });
 
-projects.addEventListener('click', function(){
+projects.addEventListener('click', function () {
     hideMobileMenu();
     smoothScroll('#projects', 500);
 });
 
-mywork.addEventListener('click', function(){
+mywork.addEventListener('click', function () {
     smoothScroll('#projects', 500);
 });
+
+const createDemo = (src) => {
+    return `<video class="img4" alt="Project Demo" autoplay controls>
+            <source src="${src}" type="video/mp4">
+            </video>`;
+}
+
+const replaceWithDemo = (element, src) => {
+    const demoHtml = createDemo(src);
+    document.getElementById(element).innerHTML = demoHtml;
+}
